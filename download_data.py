14a9bb03053526918a49c4c8d8384e59de1f1bbf
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2022-01-27 12:27:02 +0100

    Update requirements.txt

--- download_data.py
+++ download_data.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import requests, zipfile, io
 
 
-def download_data() -> None:
+def download_data() -> Path:
     zip_file_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip"
 
     local_data_dir = Path(__file__).parent / "data"
     local_data_dir.mkdir(exist_ok=True)
 
     zipfile.ZipFile(io.BytesIO(requests.get(zip_file_url).content)).extractall(local_data_dir)
+    return local_data_dir
